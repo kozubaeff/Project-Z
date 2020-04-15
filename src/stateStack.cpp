3c313d960e06f89374026711ea02f9d3d786d88a
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2020-04-15 12:01:37 +0300

    Implement handleEvent

--- src/stateStack.cpp
+++ src/stateStack.cpp
@@ -2,9 +2,19 @@
 
 #include <cassert>
 
-State::Ptr StateStack::createState(States::ID stateID) {
+State::Ptr StateStack::createState(States::ID stateID)
+{
     auto found = mFactories.find(stateID);
     assert(found != mFactories.end());
 
     return found->second();
+}
+
+void StateStack::handleEvent(const sf::Event &event)
+{
+    for (auto itr = mStack.rbegin(); itr != mStack.rend(); itr++)
+    {
+        if (!(*itr)->handleEvent(event))
+            return;
+    }
 }
