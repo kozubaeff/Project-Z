6ccf4ea7ebea30db9ad966d25c89632f4cbe8bcb
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2020-04-15 08:27:01 +0300

    Create functor Mover

--- src/player.cpp
+++ src/player.cpp
@@ -1,2 +1,25 @@
 #include "player.h"
-// --TO-DO functor
+#include "CommandQueue.h"
+#include "Hero.hpp"
+
+struct Mover
+{
+    Mover(float vx, float vy)
+            : velocity(vx, vy)
+    {
+    }
+    void operator() (SceneNode& node, sf::Time) const
+    {
+        Hero& hero = static_cast<Hero&>(node);
+        hero.setVelocity(velocity + hero.getVelocity());
+    }
+    sf::Vector2f velocity;
+};
+
+void Player::handleEvent(const sf::Event &event, CommandQueue &commands) {
+
+}
+
+void Player::handleRealtimeInput(CommandQueue &commands) {
+
+}
