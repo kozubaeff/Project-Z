3f2b6ff6a4da7da84da5feb8b926b6cd91e7f151
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2020-04-15 12:26:58 +0300

    Implement constructors/desctructors

--- src/state.cpp
+++ src/state.cpp
@@ -20,3 +20,21 @@ State::Context State::getContext() const
 {
     return mContext;
 }
+
+State::Context::Context(sf::RenderWindow& window, TextureHolder& textures, FontHolder& fonts, Player& player)
+        : window(&window)
+        , textures(&textures)
+        , fonts(&fonts)
+        , player(&player)
+{
+}
+
+State::State(StateStack& stack, Context context)
+        : mStack(&stack)
+        , mContext(context)
+{
+}
+
+State::~State()
+{
+}
