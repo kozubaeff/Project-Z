330a940af18b9d877a9459454052028b866a9b39
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2020-04-15 08:32:11 +0300

    Create derivedAction -  converter from derived to base

--- src/Command.h
+++ src/Command.h
@@ -12,4 +12,18 @@ struct Command {
     unsigned int category;
 };
 
+template <typename GameObject, typename Function>
+std::function<void(SceneNode&, sf::Time)> derivedAction(Function fn)
+{
+    return [=] (SceneNode& node, sf::Time dt)
+    {
+        // Check if cast is safe
+        assert(dynamic_cast<GameObject*>(&node) != nullptr);
+        // Downcast node and invoke function on it
+        fn(static_cast<GameObject&>(node), dt);
+    };
+}
+
+
+
 #endif //THEGAME_COMMAND_H
