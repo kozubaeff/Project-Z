cccec4d661906d990865ca27b4f167f0d35147a9
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2020-04-15 10:08:25 +0300

    Create Enum with States

---
+++ src/stateIdentifiers.h
@@ -0,0 +1,16 @@
+#ifndef THEGAME_STATEIDENTIFIERS_H
+#define THEGAME_STATEIDENTIFIERS_H
+namespace States
+{
+    enum ID
+    {
+        None,
+        Title,
+        Menu,
+        Game,
+        Loading,
+        Pause
+    };
+}
+
+#endif //THEGAME_STATEIDENTIFIERS_H
