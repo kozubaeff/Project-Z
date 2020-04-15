1843f5a41c8e9b16ff7d9415165ebabd34ae4642
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2020-04-15 07:10:47 +0300

    Use category in Command

---
+++ src/Command.cpp
@@ -0,0 +1,5 @@
+#include "Command.h"
+
+Command::Command() : category(Category::None)
+{
+}
--- src/Command.h
+++ src/Command.h
@@ -2,9 +2,14 @@
 #define THEGAME_COMMAND_H
 
 #include <SFML/System/Time.hpp>
+#include "Category.h"
+#include <functional>
+
 class SceneNode;
 struct Command {
+    Command();
     std::function<void(SceneNode&, sf::Time)> action;
+    unsigned int category;
 };
 
 #endif //THEGAME_COMMAND_H
