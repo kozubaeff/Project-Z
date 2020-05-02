62f93910543eae9914c4c1a069f16bfc8d0b0d2b
egiptipavel <kinqstone585@gmail.com>
2020-05-02 19:16:15 +0300

    Add tiles and textures of map

---
+++ src/tiles/Slates.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Slates" tilewidth="32" tileheight="32" tilecount="1104" columns="48">
+ <image source="../images/Slates.png" width="1536" height="736"/>
+</tileset>
